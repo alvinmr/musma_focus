5c04edf8344f38cad81ae0fa584a254763cdad5e
Alvin <maulanaalvin1@gmail.com>
2021-11-12 00:08:25 +0800

    clean up views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,36 +1,62 @@
-@extends('layouts.app')
+@php
+$configData = Helper::applClasses();
+@endphp
+@extends('layouts/fullLayoutMaster')
+
+@section('title', 'Masuk')
+
+@section('page-style')
+    {{-- Page Css files --}}
+    <link rel="stylesheet" href="{{ asset(mix('css/base/plugins/forms/form-validation.css')) }}">
+    <link rel="stylesheet" href="{{ asset(mix('css/base/pages/authentication.css')) }}">
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="auth-wrapper auth-cover">
+        <div class="m-0 auth-inner row">
+            <!-- Brand logo-->
+            <a class="brand-logo" href="#">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
+                <h2 class="brand-text text-primary ms-1">Musma Focus</h2>
+            </a>
+            <!-- /Brand logo-->
+
+            <!-- Left Text-->
+            <div class="p-5 d-none d-lg-flex col-lg-8 align-items-center">
+                <div class="px-5 w-100 d-lg-flex align-items-center justify-content-center">
+                    @if ($configData['theme'] === 'dark')
+                        <img class="img-fluid" src="{{ asset('images/pages/login-v2-dark.svg') }}" alt="Login V2" />
+                    @else
+                        <img class="img-fluid" src="{{ asset('images/pages/login-v2.svg') }}" alt="Login V2" />
+                    @endif
+                </div>
+            </div>
+            <!-- /Left Text-->
+
+            <!-- Login-->
+            <div class="px-2 d-flex col-lg-4 align-items-center auth-bg p-lg-5">
+                <div class="mx-auto col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2">
+                    <h2 class="mb-1 card-title fw-bold">Selamat Datang di Website Musma Focus! 👋</h2>
+                    <p class="mb-2 card-text">Silahkan masuk ke akun kamu terlebih dahulu</p>
+                    <form class="mt-2 auth-login-form" action="{{ route('login') }}" method="POST">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="mb-1">
+                            <label class="form-label" for="email">Email</label>
+                            <input class="form-control @error('email') is-invalid @enderror" id="email" type="text"
+                                name="email" placeholder="john@example.com" aria-describedby="email" autofocus=""
+                                tabindex="1" value="{{ old('email') }}" />
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+                        <div class="mb-1">
+                            <div class="input-group input-group-merge form-password-toggle">
+                                <input class="form-control form-control-merge @error('password') is-invalid @enderror"
+                                    id="password" type="password" name="password" placeholder="············"
+                                    aria-describedby="password" tabindex="2" />
+                                <span class="cursor-pointer input-group-text"><i data-feather="eye"></i></span>
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -38,36 +64,17 @@
                                 @enderror
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
+                        <div class="mb-1">
+                            <div class="form-check">
+                                <input class="form-check-input" id="remember-me" type="checkbox" tabindex="3" />
+                                <label class="form-check-label" for="remember-me"> Ingat saya</label>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
+                        <button class="btn btn-primary w-100" tabindex="4">Masuk</button>
                     </form>
                 </div>
             </div>
+            <!-- /Login-->
         </div>
     </div>
-</div>
 @endsection
