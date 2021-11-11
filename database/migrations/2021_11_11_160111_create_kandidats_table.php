8869c8bb5065b6e7030bb23558e4b067ee881a44
Alvin <maulanaalvin1@gmail.com>
2021-11-12 00:08:07 +0800

    prepare for candidate modul

---
+++ database/migrations/2021_11_11_160111_create_kandidats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKandidatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kandidat', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('angkatan');
+            $table->string('nim');
+            $table->string('fakultas');
+            $table->string('prodi');
+            $table->string('deskripsi');
+            $table->string('visi');
+            $table->string('misi');
+            $table->string('foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kandidats');
+    }
+}
