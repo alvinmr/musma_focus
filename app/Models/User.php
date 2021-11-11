acc15e1c7070a8d252433270f19fe654bcdc3345
Alvin <maulanaalvin1@gmail.com>
2021-11-12 00:08:39 +0800

    setting user model to use role

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -18,6 +20,9 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'nim',
+        'prodi',
+        'fakultas',
         'email',
         'password',
     ];
