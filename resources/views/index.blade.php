9f9b08dd02147d100786bf645c0bf9751fa33334
dyudhani <ddickyumardhani@gmail.com>
2021-11-28 16:31:41 +0800

    dicoba dulu itu udah aman di frotn end aja

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,20 +14,19 @@ $configData = Helper::applClasses();
 </section> --}}
 
 <!-- ======= Header ======= -->
-<header id="header" class="header fixed-top">
+<header id="navbar" class="header fixed-top">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
 
-      <a href="#" class="logo d-flex align-items-center">
-        {{-- <img src="assets/img/logo.png" alt=""> --}}
+      <a href="#" class="logo d-flex align-items-center"> 
         <span>MUSMA FOCUS</span>
       </a>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-          <li><a class="nav-link scrollto" href="#hero">Tentang</a></li>
-          <li><a class="nav-link scrollto" href="#services">Polling</a></li>
-          <li><a class="nav-link scrollto" href="#contact">Contact Us</a></li>
+          <li><a class="nav-link scrollto active" href="#header">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">Tentang</a></li>
+          <li><a class="nav-link scrollto" href="#polling">Polling</a></li>
+          <li><a class="nav-link scrollto" href="#footer">Contact Us</a></li>
           <li><a class="getstarted scrollto" href="/login">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
@@ -36,7 +35,7 @@ $configData = Helper::applClasses();
     </div>
 </header><!-- End Header -->
 
-<section id="Tentang" class="hero d-flex align-items-center">
+<section id="header" class="hero d-flex align-items-center">
     <div class="container">
         <div class="row">
           <div class="col-lg-6 d-flex flex-column justify-content-center">
@@ -44,7 +43,7 @@ $configData = Helper::applClasses();
             <h2 data-aos="fade-up" data-aos-delay="400">"Stay Focus, Stay On Top"</h2>
             <div data-aos="fade-up" data-aos-delay="600">
               <div class="text-center text-lg-start">
-                <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+                <a href="#polling" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
                   <span>Mulai Polling</span>
                   <i class="bi bi-arrow-right"></i>
                 </a>
@@ -81,8 +80,8 @@ $configData = Helper::applClasses();
     </section><!-- End About Section -->
 
 
-    <!-- ======= Pricing Section ======= -->
-    <section id="pricing" class="pricing">
+    <!-- ======= polling Section ======= -->
+    <section id="polling" class="pricing">
 
       <div class="container" data-aos="fade-up">
 
@@ -97,7 +96,7 @@ $configData = Helper::applClasses();
             <div class="box">
               <h1 style="color: #07d5c0;">Dicky Umardhani</h1>
               <img src="{{ asset('images/avatars/3.png') }}" class="img-fluid" alt="">
-              <a href="#" class="btn-buy">Lihat Visi & Misi</a>
+              <button href="#" class="btn-buy" data-bs-toggle="modal" data-bs-target="#modalVisiMisi">Lihat Visi & Misi</button>
             </div>
           </div>
 
@@ -105,7 +104,7 @@ $configData = Helper::applClasses();
             <div class="box">
               <h1 style="color: #65c600;">Dimas Aldi</h1>
               <img src="{{ asset('images/avatars/3.png') }}" class="img-fluid" alt="">
-              <a href="#" class="btn-buy">Lihat Visi & Misi</a>
+              <button href="#" class="btn-buy" data-bs-toggle="modal" data-bs-target="#modalVisiMisi">Lihat Visi & Misi</button>
             </div>
           </div>
 
@@ -113,7 +112,7 @@ $configData = Helper::applClasses();
             <div class="box">
               <h1 style="color: #ff901c;">Rizky Chandra</h1>
               <img src="{{ asset('images/avatars/3.png') }}" class="img-fluid" alt="">
-              <a href="#" class="btn-buy">Lihat Visi & Misi</a>
+              <button href="#" class="btn-buy" data-bs-toggle="modal" data-bs-target="#modalVisiMisi">Lihat Visi & Misi</button>
             </div>
           </div>
 
@@ -121,68 +120,81 @@ $configData = Helper::applClasses();
             <div class="box">
               <h1 style="color: #ff0071;">Desak Mulya</h1>
               <img src="{{ asset('images/avatars/3.png') }}" class="img-fluid" alt="">
-              <a href="#" class="btn-buy">Lihat Visi & Misi</a>
+              <button href="#" class="btn-buy" data-bs-toggle="modal" data-bs-target="#modalVisiMisi">Lihat Visi & Misi</button>
             </div>
           </div>
 
-        </div>
+          <div class="text-center text-lg-start d-grid gap-2 col-lg-3 mx-auto">
+            <a href="#about" class="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
+              <span>Mulai Polling</span>
+              <i class="bi bi-arrow-right"></i>
+            </a>
+          </div>
 
+        </div>
       </div>
 
     </section><!-- End Pricing Section -->
 
 </main><!-- End #main -->
 
+
+{{-- Modal Visi dan Misi --}}
+<div class="modal fade" id="modalVisiMisi" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Visi & Misi</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        asdasdasdasd
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
 
-    <div class="footer-top">
+  <div class="footer-top">
     <div class="container">
-        <div class="row gy-4">
-            <div class="col-lg-5 col-md-12 footer-info">
-                <a href="index.html" class="logo d-flex align-items-center">
-                    <img src="assets/img/logo.png" alt="">
-                    <span>MUSMA FOCUS</span>
-                </a>
-                <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-                </div>
-            </div>
-
-        <div class="col-lg-3 col-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Home</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Tentang</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Polling</a></li>
-            <li><i class="bi bi-chevron-right"></i> <a href="#">Contact User</a></li>
-            </ul>
+      <div class="row gy-4">
+        <div class="col-lg-5 col-md-12 footer-info">
+          <a href="index.html" class="logo d-flex align-items-center">
+              <img src="assets/img/logo.png" alt="">
+              <span>MUSMA FOCUS</span>
+          </a>
+          <div class="social-links mt-3">
+            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+          </div>
         </div>
 
         <div class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
-            <h4>Contact Us</h4>
-            <p>
-            Dicky <br>
-            Denpasar, 28 August 2001<br>
-            Bali <br><br>
-            <strong>Phone:</strong> +1 5589 55488 55<br>
-            <strong>Email:</strong> info@example.com<br>
-            </p>
-
+          <h4>Contact Us</h4>
+          <p>
+          Dicky <br>
+          Denpasar, 28 August 2001<br>
+          Bali <br><br>
+          <strong>Phone:</strong> +1 5589 55488 55<br>
+          <strong>Email:</strong> info@example.com<br>
+          </p>
         </div>
+      </div>
+    </div>
+  </div>
 
-        </div>
+  <div class="container">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Dicky Umardhani</span></strong>. All Rights Reserved
     </div>
-    </div>
-
-    <div class="container">
-        <div class="copyright">
-            &copy; Copyright <strong><span>Dicky Umardhani</span></strong>. All Rights Reserved
-        </div>
-    </div>
+  </div>
 </footer><!-- End Footer -->
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
