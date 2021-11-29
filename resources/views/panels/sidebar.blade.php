68108672651f6bcd49c6284debe0dd702eb323e1
alvinmr <maulanaalvin1@gmail.com>
2021-11-29 21:00:13 +0800

    fix sidebar buat semua

--- resources/views/panels/sidebar.blade.php
+++ resources/views/panels/sidebar.blade.php
@@ -64,9 +64,9 @@ $configData = Helper::applClasses();
                 @endif
                 {{-- Foreach menu item ends --}}
             @else
-                <li class="nav-item"><a class="nav-link d-flex align-items-center @if (Request::is('/')) active @endif"
+                <li class="nav-item @if (Request::is('/')) active @endif"><a class="nav-link d-flex align-items-center "
                         href="{{ route('home') }}"><i data-feather="home"></i><span>Dashboards</span></a></li>
-                <li class="nav-item"><a class="nav-link d-flex align-items-center @if (Request::is('voting')) active @endif"
+                <li class="nav-item @if (Request::is('voting')) active @endif"><a class="nav-link d-flex align-items-center "
                         href="{{ route('voting') }}"><i data-feather="user-check"></i><span>Voting</span></a></li>
             @endrole
 
