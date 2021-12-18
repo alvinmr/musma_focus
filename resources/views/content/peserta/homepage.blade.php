0e8a97be1bfdeae07a519a2dcd2424a391edab24
Alvin <maulanaalvin1@gmail.com>
2021-12-18 08:38:13 +0800

    hapus chart

--- resources/views/content/peserta/homepage.blade.php
+++ resources/views/content/peserta/homepage.blade.php
@@ -26,7 +26,7 @@
     </div>
     <!--/ Kick start -->
 
-    <div class="justify-content-center row">
+    {{-- <div class="justify-content-center row">
         <div class="col-lg-12 col-md-12 col-sm-12">
             <div class="card">
                 <div class="card-header">
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 @endsection
 
 @section('page-script')
